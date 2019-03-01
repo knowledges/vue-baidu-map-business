f784cdb4c0088abfcacb705f7bb322e0b7665a3c
knowledges <qbl378739311@163.com>
2019-03-01 15:11:20 +0800

    实例
    全局注册

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import BaiduMap from './index'
 
 Vue.config.productionTip = false
-
+Vue.use(BaiduMap, {
+  ak: 'sFGGCnZu8HcewIdGMFGGaGypsfILG36G'
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
