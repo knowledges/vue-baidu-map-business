0b1d110df7bc3f4ff154c473c9b35d200529093e
knowledges <qbl378739311@163.com>
2019-03-01 15:10:59 +0800

    注册组件
    1.BmView
    2.BmScale
    3.BmNavigation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import BaiduMap from './components/map/Map.vue'
+import BmView from './components/map/MapView.vue'
+import BmScale from './components/controls/Scale.vue'
+import BmNavigation from './components/controls/Navigation.vue'
 
 export default {
   install (Vue, opt) {
     const { ak } = opt
     Vue.prototype._BMap = () => ({ak})
-
     Vue.component('baidu-map', BaiduMap)
+    Vue.component('bm-view', BmView)
+    Vue.component('bm-scale', BmScale)
+    Vue.component('bm-navigation', BmNavigation)
   }
 }
 
-export { BaiduMap }
+export { BaiduMap, BmView, BmScale, BmNavigation }
