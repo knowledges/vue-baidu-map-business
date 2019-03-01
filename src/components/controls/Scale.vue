081cf8137950f4f837eba7314fe6907da37d1ccd
knowledges <qbl378739311@163.com>
2019-03-01 15:08:11 +0800

    控件
    距离尺

---
+++ src/components/controls/Scale.vue
@@ -0,0 +1,36 @@
+<!--qiu.bl-->
+<template></template>
+<script>
+import commonMixin from '../base/mixins/common'
+import { CreateSize } from '../base/mixins/factory'
+export default {
+  name: 'bm-scale',
+  mixins: [commonMixin('control')],
+  props: {
+    anchor: {
+      type: String
+    },
+    offset: {
+      type: Object
+    }
+  },
+  watch: {
+    anchor () {
+      this.reload()
+    },
+    offset () {
+      this.reload()
+    }
+  },
+  methods: {
+    load () {
+      const {BMap, map, anchor, offset} = this
+      this.SCALE_DIRECTION = new BMap.ScaleControl({
+        anchor: global[anchor],
+        offset: offset && CreateSize(BMap ,offset)
+      })
+      map.addControl(this.SCALE_DIRECTION)
+    }
+  }
+}
+</script>
