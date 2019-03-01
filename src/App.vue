c5e7e908b7e8de225cd08d96aecdc410551f2bd9
knowledges <qbl378739311@163.com>
2019-03-01 15:07:01 +0800

    显示组件功能
    1.演示地图
    2.演示地图view 层
    3.演示距离尺
    4.演示平移缩放控件

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <baidu-map class="bm-view" :center="'南京'" :zoom="15">
+      <!-- map -->
+      <bm-view style="width: 100%; height: 100%; flex: 1;"></bm-view>
+      <bm-scale :anchor="'BMAP_ANCHOR_BOTTOM_LEFT'" :offset="{width: 0, height: 50}"></bm-scale>
+      <bm-navigation :anchor="'BMAP_ANCHOR_TOP_LEFT'" :type="'BMAP_NAVIGATION_CONTROL_LARGE'"></bm-navigation>
+    </baidu-map>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+import BaiduMap from './components/map/Map.vue'
+import BmView from './components/map/MapView.vue'
+import BmScale from './components/controls/Scale.vue'
+import BmNavigation from './components/controls/Navigation.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BaiduMap, BmView, BmScale, BmNavigation
+  },
+  mounted () {
+
   }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.bm-view {
+  width: 100%;
+  height: 620px;
 }
 </style>
